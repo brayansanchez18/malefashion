166be75ec15663aae6f320ce61b3e50c03edbf28
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-09-26 21:11:34 -0600

    slide dinamico seguimos con productos destacados

--- frontend/vistas/modulos/slide.php
+++ frontend/vistas/modulos/slide.php
@@ -1,54 +1,39 @@
+<?php
+$slide = ControladorSlide::ctrMostrarSlide();
+$social = ControladorPlantilla::ctrEstiloPlantilla();
+$jsonRedesSociales = json_decode($social['redesSociales'], true);
+?>
+
 <!-- Hero Section Begin -->
 <section class="hero">
   <div class="hero__slider owl-carousel">
-    <div class="hero__items set-bg" data-setbg="<?= $backend ?>vistas/img/hero/hero-1.jpg">
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-5 col-lg-7 col-md-8">
-            <div class="hero__text">
-              <h6>Summer Collection</h6>
-              <h2>Fall - Winter Collections 2030</h2>
-              <p>
-                A specialist label creating luxury essentials. Ethically
-                crafted with an unwavering commitment to exceptional
-                quality.
-              </p>
-              <a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
-              <div class="hero__social">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-pinterest"></i></a>
-                <a href="#"><i class="fa fa-instagram"></i></a>
+
+    <?php foreach ($slide as $key => $value) : ?>
+      <div class="hero__items set-bg" data-setbg="<?= $backend . $value['imgFondo'] ?>">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-5 col-lg-7 col-md-8">
+              <div class="hero__text">
+                <h6><?= $value['titulo1'] ?></h6>
+                <h2><?= $value['titulo2'] ?></h2>
+                <p>
+                  <?= $value['titulo3'] ?>
+                </p>
+                <a href="<?= $value['url'] ?>" class="primary-btn"><?= $value['boton'] ?><span class="arrow_right"></span></a>
+                <div class="hero__social">
+
+                  <?php foreach ($jsonRedesSociales as $key => $value2) : ?>
+                    <?php if ($value2['activo'] != 0) : ?>
+                      <a href="<?= $value2['url'] ?>"><i class="fa <?= $value2['red'] ?>"></i></a>
+                    <?php endif ?>
+                  <?php endforeach ?>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="hero__items set-bg" data-setbg="<?= $backend ?>vistas/img/hero/hero-2.jpg">
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-5 col-lg-7 col-md-8">
-            <div class="hero__text">
-              <h6>Summer Collection</h6>
-              <h2>Fall - Winter Collections 2030</h2>
-              <p>
-                A specialist label creating luxury essentials. Ethically
-                crafted with an unwavering commitment to exceptional
-                quality.
-              </p>
-              <a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
-              <div class="hero__social">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-pinterest"></i></a>
-                <a href="#"><i class="fa fa-instagram"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php endforeach ?>
   </div>
 </section>
 <!-- Hero Section End -->
